44efdeae8a057005e1be4dec535a67b7b6715b34
左思 <153209088@qq.com>
2015-11-30 19:19:11 +0800

    Initial Commit

---
+++ nstimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nstimer
+//
+//  Created by 杨元锋 on 15/11/30.
+//  Copyright © 2015年 netcore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ nstimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  nstimer
+//
+//  Created by 杨元锋 on 15/11/30.
+//  Copyright © 2015年 netcore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
